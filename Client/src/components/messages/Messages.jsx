921b8694d767d0b2dbdd694a60d5939eea7c9a20
Pedro Kretikouski Roque Junior <pedro.junior@ringa.com.br>
2020-02-17 12:04:39 -0300

    Corrige Scroll 'Scroll rola automaticamente para apresentar a ultima mensagem na view'

--- Client/src/components/messages/Messages.jsx
+++ Client/src/components/messages/Messages.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
+import ReactDOM from 'react-dom'
 
 import Message from './message/Message'
 import './messages.sass'
 
 const Messages = ({ messages, name }) => {
+
+    const boxChat = useRef()
+
+    useEffect(() => {
+        const node = ReactDOM.findDOMNode(boxChat.current)
+        node.scrollTop = node.scrollHeight
+    })
+
     return (
-        <div className="container-messages">
+        <div className="container-messages" ref={boxChat}>
             <div className="messages">
                 {messages.map((message, index) => <div key={index}><Message message={ message } name={ name } /></div>)}
             </div>
